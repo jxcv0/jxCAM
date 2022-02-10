b03e5ea245808127fb888bc225e9bac7e5b8003b
jxcv <jmicoud02@gmail.com>
2022-02-10 21:42:48 +0000

    Add file opening

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,10 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QFileDialog>
+#include <QDebug>
+#include <QMessageBox>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -11,5 +15,32 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+/**
+ * @brief MainWindow::on_pushButton_open_clicked
+ * Open a program and proccess its contents
+ */
+void MainWindow::on_pushButton_open_clicked()
+{
+    QFileDialog dialog(this);
+    dialog.setFileMode(QFileDialog::AnyFile);
+    dialog.setNameFilter(tr("PAF Files (*.PAF)"));
+    dialog.setViewMode(QFileDialog::Detail);
+    QString filename;
+    if (dialog.exec()) {
+        if (dialog.selectedFiles().size() > 1) {
+            QMessageBox::critical(this, "Open file", "Only one file can be opened at a time");
+        }
+        filename = dialog.selectedFiles().front();
+    }
+    QFile file(filename);
+    if (!file.open(QIODevice::ReadWrite | QIODevice::Text))
+        return;
+
+    while (!file.atEnd()) {
+        QByteArray line = file.readLine();
+        qDebug() << line << "\n";
+    }
 }
 
